e2ceff3b10f8b88ef74b173991dec6e479e81845
Max Flowers <81179442+MaxFlowers333@users.noreply.github.com>
2021-11-04 08:59:16 +0000

    clea coat

--- src/sculpture.js
+++ src/sculpture.js
@@ -11,9 +11,11 @@ class Sculpture extends THREE.Object3D{
 
       this.options = {
         transmission: 1,
-        thickness: 0.5,
-        roughness: 0.07,
-        envMapIntensity: 1.5
+        thickness: 1.5,
+        roughness: 0.6,
+        envMapIntensity: 1.5,
+        clearcoat: 1,
+        clearcoatRoughness: 0.1
       }
 
     
@@ -33,28 +35,39 @@ class Sculpture extends THREE.Object3D{
         transmission: this.options.transmission,
         thickness: this.options.thickness,
         envMap: hdrEquirect,
-        envMapIntensity: this.options.envMapIntensity
+        envMapIntensity: this.options.envMapIntensity,
+        clearCoat: this.options.clearCoat,
+        clearCoatRoughness: this.options.clearCoatRoughness
       });
       gui.add(this.options, 'transmission', 0, 1, 0.01).onChange((val)=>{
         this.material.transmission = this.options.transmission
       })
-      gui.add(this.options, 'thickness', 0, 1, 0.01).onChange((val)=>{
+      gui.add(this.options, 'thickness', 0, 5, 0.1).onChange((val)=>{
         this.material.thickness = this.options.thickness;
       })
       gui.add(this.options, 'roughness', 0, 1, 0.01).onChange((val)=>{
         this.material.roughness = this.options.roughness;
       })
+      gui.add(this.options, "envMapIntensity", 0, 3, .1).onChange((val)=>{
+        this.material.envMapIntensity = this.options.envMapIntensity = this.options.envMapIntensity;
+      })
+      gui.add(this.options, 'clearcoat', 0, 1, 0.01).onChange((val)=>{
+        this.material.clearcoat = val;
+      });
+      gui.add(this.options, 'clearcoatRoughness', 0, 1, 0.01).onChange((val)=>{
+        this.material.clearcoatRoughness = val;
+      });
       this.palette = colors.getRandom(5);
      
 
-      const geometry = new THREE.IcosahedronBufferGeometry(0.25, 0);
+      const geometry = new THREE.IcosahedronBufferGeometry(0.25, 25);
 
 
       this.mesh = new THREE.Mesh(geometry, this.material);
       this.mesh.position.set(5, 0.3, 5);
       this.add(this.mesh);
 
-      let light = new THREE.DirectionalLight(0xfff0dd, 1)
+      let light = new THREE.DirectionalLight(0xfff0dd, 10)
       light.position.set(0, 10, 5);
       this.add(light)
 
